a34292e9c8d8e2a32f3ca0fb8ef5498481508141
Tuan IB. Nguyen Ngo Anh <tuannna@user.techcombank.com.vn>
2024-07-12 09:35:29 +0700

    changes

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__)
 
-conn = psycopg2.connect(database="mydatabase", user="myuser", password="mypassword", host="localhost", port=8269)
+conn = psycopg2.connect(database="mydatabase", user="myuser", password="mypassword", host="db", port=8269)
 
 class Account:
     def __init__(self, array):
